c631b785de65eb93550974bb8414a1e25d88a214
raghu-rajzs <raghuraj.muni@zs.com>
2024-09-11 18:07:12 +0530

    updated header to tabs

--- src/app/global-header/global-header.component.ts
+++ src/app/global-header/global-header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'app-global-header',
   standalone: true,
-  imports: [NzMenuModule],
+  imports: [NzTabsModule, NzAvatarModule, NzBadgeModule],
   templateUrl: './global-header.component.html',
-  styleUrl: './global-header.component.css'
+  styleUrl: './global-header.component.css',
 })
-export class GlobalHeaderComponent {
-
-}
+export class GlobalHeaderComponent {}
