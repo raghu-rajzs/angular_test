510020253e68be6516bc74f3bb0ab6bf634e69c5
Snideep Mondal <snideep.mondal@zs.com>
2024-09-12 15:26:47 +0530

    Charts Added

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
-import { ChartConfiguration } from 'chart.js';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-charts',
@@ -11,11 +11,7 @@ import { ChartConfiguration } from 'chart.js';
 })
 export class ChartsComponent {
 
-  title = 'ng2-charts-demo';
-
-  public barChartLegend = true;
-  public barChartPlugins = [];
-
+  //Bar Chart
   public barChartData: ChartConfiguration<'bar'>['data'] = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
@@ -23,12 +19,44 @@ export class ChartsComponent {
       { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
     ]
   };
-
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
   };
 
-  constructor() {
-  }
+
+ // Pie Chart
+  public pieChartLabels = [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ];
+  public pieChartDatasets = [ {
+    data: [ 300, 500, 100 ]
+  } ];
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+
+  // Line Chart
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgb(253, 218, 13)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false
+  };
 
 }
