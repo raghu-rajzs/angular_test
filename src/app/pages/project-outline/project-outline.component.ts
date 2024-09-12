2288c4faa8687c7bebcd635698710324669d1436
raghu-rajzs <raghuraj.muni@zs.com>
2024-09-12 16:13:22 +0530

    minor update

--- src/app/pages/project-outline/project-outline.component.ts
+++ src/app/pages/project-outline/project-outline.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChartsComponent } from "../../components/charts/charts.component";
 
 @Component({
   selector: 'app-project-outline',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChartsComponent],
   templateUrl: './project-outline.component.html',
   styleUrl: './project-outline.component.css'
 })
